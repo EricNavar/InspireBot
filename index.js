e1eabc8e9aa0d75812aa4aa55e1952731e4e33a8
EricNavar <ericnavar@ufl.edu>
2020-10-24 13:38:58 -0400

    fix settings for browser to run on heroku

--- index.js
+++ index.js
@@ -19,9 +19,8 @@ bot.on("message", message => {
     
       try {
         const argies = [
-          "--no-sandbox",
-          '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"',
-          "--start-maximized"
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
         ];
         const options = {
           args: argies
